a05b73b0b8550203c5d4e5a7ee1b7c797eee9bd4
Ig Momin Khan <mominkhan8991@gmail.com>
2023-04-10 10:37:48 +0300

    banner area complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,52 @@
 import { useState } from 'react';
 import { Link } from "react-router-dom";
-import { Bars3BottomRightIcon } from '@heroicons/react/24/outline'
+import { Bars3BottomRightIcon,XMarkIcon } from '@heroicons/react/24/outline'
 
 
 export default function Header() {
     const [isOpen, setIsOpen] = useState();
-    return (<header className="bg-gradient-to-r from-accent/5 to-primary/5 bg-sky-500/05">
-        
-        <div className="mx-auto w-11/12  px-4 py-8 relative">
-            <nav className="flex items-center justify-between">
-                {/* brand name / logo */}
-                <h1 className="font-bold text-4xl"><Link to='/'>Dream Job</Link></h1>
-                {/* hamburger icon hidden on large device*/}
-                <div className="w-8 h-8 lg:hidden" onClick={() => setIsOpen(prevState => !prevState)}><Bars3BottomRightIcon /></div>
-                {/* navigation links */}
-                <ul className={`text-center text-xl text-dark03 flex gap-4 lg:gap-8 flex-col lg:flex-row lg:flex absolute lg:static lg:w-[auto] ${isOpen ? 'right-auto top-24 w-[calc(100%-2rem)]' : 'hidden w-auto -right-full'}`}>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='statistics'>Statistics</Link>
-                    </li>
-                    <li>
-                        <Link to='applied-jobs'>Applied Jobs</Link>
-                    </li>
-                    <li>
-                        <Link to='blog'>Blog</Link>
-                    </li>
-                    <li className='lg:hidden'>
-                        <div className="gradient-btn">
-                            <Link to='#'>Start Applying</Link>
-                        </div>
-                    </li>
-                </ul>
-            
-                {/* apply button hidden on small devices */}
-            
-                <div className="gradient-btn hidden lg:block">
-                    <Link to='#'>Start Applying</Link>
-                </div>
-            </nav>
-        </div>
+    return (
+        <header className="bg-gradient-to-r from-accent/5 to-primary/5">
 
-    </header>
+            <div className="mx-auto w-11/12  px-4 py-8 relative">
+                <nav className={`flex items-center justify-between`}>
+
+                    {/* brand name / logo */}
+
+                    <h1 className="font-bold text-4xl"><Link to='/'>Dream Job</Link></h1>
+
+                    {/* hamburger icon hidden on large device*/}
+
+                    <div className="w-8 h-8 lg:hidden" onClick={() => setIsOpen(prevState => !prevState)}>{isOpen ? <XMarkIcon /> : <Bars3BottomRightIcon />}</div>
+
+                    {/* navigation links */}
+
+                    <ul className={`text-center text-xl text-dark03 flex gap-4 lg:gap-8 flex-col lg:flex-row lg:flex fixed lg:static lg:w-[auto] ${isOpen ? 'bg-slate-700 rounded-lg text-white font-bold p-8 right-auto top-32 w-[calc(100vw-12%)] p-8' : 'hidden w-auto -right-full'}`}>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='statistics'>Statistics</Link>
+                        </li>
+                        <li>
+                            <Link to='applied-jobs'>Applied Jobs</Link>
+                        </li>
+                        <li>
+                            <Link to='blog'>Blog</Link>
+                        </li>
+                        <li className='lg:hidden'>
+                            <Link className='gradient-btn inline-block' to='#'>Start Applying</Link>
+                        </li>
+                    </ul>
+
+                    {/* apply button hidden on small devices */}
+
+                    <div className="hidden lg:block">
+                        <Link className='gradient-btn' to='#'>Start Applying</Link>
+                    </div>
+                </nav>
+            </div>
+
+        </header>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import Hiring from '../assets/animation/hiring.json';
 
+const Home = () => {
+    return (
+        <main className='w-11/12 mx-auto '>
+            <div className="min-h-[calc(100vh-7rem)] flex items-center justify-center">
 
-const Home = ()=> {
-    return <h1>Home</h1>
+                <div className="flex flex-col lg:flex-row gap-6 py-16">
+                    <div className='text-center flex flex-col  justify-center lg:text-left flow-content flex-1 p'>
+                        <h1 className='text-[clamp(2.5rem,1.75rem+3vw,4.5rem)] leading-[1.15] md: font-extrabold lg:max-w-[10ch] max-w-[15ch] mx-auto lg:mx-0'>One Step closer to your <strong className='bg-gradient bg-clip-text text-transparent'>deam job</strong></h1>
+                        <p className='max-w-[45ch] mx-auto text-dark03 text-lg lg:mx-0'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                        <Link className='gradient-btn lg:self-start self-center' to='#jobs'>Get Started</Link>
+                    </div>
+                    <div className='flex-1'>
+                        <Lottie animationData={Hiring} />
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default Home;
